a00c36f5240e37ae7edc8bcac789cd4aaad56eb9
Beonnica Mina <beonnicamina@gmail.com>
2026-04-05 19:25:57 +0800

    reference classes [TBF]

---
+++ src/LeaveRequest.java
@@ -0,0 +1,2 @@
+public class LeaveRequest {
+}
---
+++ src/SystemUser.java
@@ -0,0 +1,11 @@
+public class SystemUser {
+    private String name;
+
+    public SystemUser() {}
+
+    public SystemUser(String name) {
+        this.name = name;
+    }
+
+    public String getName() {return name;}
+}
